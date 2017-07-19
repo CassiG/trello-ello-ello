a2f921477d38f2431d0fd6aa13da105f5dd585e3
Cassi Gallagher <email": null>
2017-07-19 15:27:57 -0700

    Add homepage user story

--- spec/features/signing_up_for_an_account_feature_spec.rb
+++ spec/features/signing_up_for_an_account_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'signing up for an account' do
   scenario 'the user visits the registration form from the home page' do
-    visit '/'
+    visit boards_path
     click_link('Sign Up')
 
     expect(page).to have_current_path new_user_path
