d79b16d26833e69c3d086d7cc94b9f1f772b40c5
Cassi Gallagher <email": null>
2017-07-19 11:07:10 -0700

    Configure shoulda-matchers gem in spec_helper, create feature test and database

---
+++ spec/features/signing_up_for_an_account_feature_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'signing up for an account' do
+  senario 'the user visits the registration form from the home page' do
+    visit '/'
+    click_link('Sign Up')
+
+    expect(page).to have_current_path new_user_path
+  end
+end
